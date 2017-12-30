09de3ee6723dd2ad17252098ed361dc6f0cf276e
Shahar <shaharman5@gmail.com>
2017-12-30 21:42:23 +0200

    veterinary clinics list + detailed clinic activity

---
+++ app/src/main/java/il/ac/tau/cloudweb17a/hasorkim/DataParser.java
@@ -0,0 +1,215 @@
+package il.ac.tau.cloudweb17a.hasorkim;
+
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataParser {
+    private static final int WEEKDAYS_NUMBER = 7;
+    private static final int SUNDAY = 0;
+    private static final int MONDAY = 1;
+    private static final int TUESDAY = 2;
+    private static final int WEDNESDAY = 3;
+    private static final int THURSDAY = 4;
+    private static final int FRIDAY = 5;
+    private static final int SATURDAY = 6;
+    private static final String TAG = DataParser.class.getSimpleName();
+
+    static List<VeterinaryClinic> parsePlaces(String jsonData) {
+        List<VeterinaryClinic> vetsList = new ArrayList<>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+            int placesCount = jsonArray.length();
+
+            for (int i = 0; i < placesCount; i++)
+                vetsList.add(getPlace((JSONObject) jsonArray.get(i)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Vet list size is: " + vetsList.size());
+        return vetsList;
+    }
+
+    private static VeterinaryClinic getPlace(JSONObject googlePlaceJson) {
+        VeterinaryClinic vetPlace = null;
+        String place_id = null;
+        String placeName = null;
+        String vicinity = null;
+
+        try {
+            if (!googlePlaceJson.isNull("place_id"))
+                place_id = googlePlaceJson.getString("place_id");
+
+            if (!googlePlaceJson.isNull("name"))
+                placeName = googlePlaceJson.getString("name");
+
+            if (!googlePlaceJson.isNull("vicinity"))
+                vicinity = googlePlaceJson.getString("vicinity");
+
+            // TODO delete this
+            /*
+            if (!googlePlaceJson.isNull("opening_hours")) {
+                googlePlaceJson = googlePlaceJson.getJSONObject("opening_hours");
+
+                if ((!googlePlaceJson.isNull("open_now")) && (googlePlaceJson.getString("open_now").equals("true")))
+                    openNow = true;
+            }
+            */
+
+            vetPlace = new VeterinaryClinic(place_id, placeName, vicinity);
+            Log.d(TAG, vetPlace.getPlaceId());
+            Log.d(TAG, vetPlace.getName());
+            Log.d(TAG, vetPlace.getAddress());
+        } catch (JSONException e) {
+            Log.d(TAG, "Error");
+            e.printStackTrace();
+        }
+
+        return vetPlace;
+    }
+
+    static void getDistances(String jsonData, List<VeterinaryClinic> vetList) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray("rows").getJSONObject(0).getJSONArray("elements");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject element = (JSONObject)(jsonArray.get(i));
+                String distance = element.getJSONObject("duration").getString("value");
+                vetList.get(i).setDistanceFromOrigin(Math.round(Double.parseDouble(distance) / 60));
+            }
+
+            Collections.sort(vetList); // sort according to distance
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Map<String, Object> parsePlaceDetails(String jsonData) {
+        Map<String, Object> placeDetails = new HashMap<>();
+        String vetLatitude = null, vetLongitude = null, phoneNumber = null;
+        String[] openingHours = null;
+        String website = null;
+
+        try {
+            JSONObject placeDetailsJSON = (new JSONObject(jsonData)).getJSONObject("result");
+
+            if (!placeDetailsJSON.isNull("geometry")) {
+                JSONObject LatLngJSON = placeDetailsJSON.getJSONObject("geometry");
+
+                if (!LatLngJSON.isNull("location")) {
+                    LatLngJSON = LatLngJSON.getJSONObject("location");
+
+                    if (!LatLngJSON.isNull("lat") && !LatLngJSON.isNull("lng")) {
+                        vetLatitude = LatLngJSON.getString("lat");
+                        vetLongitude = LatLngJSON.getString("lng");
+                    }
+                }
+            }
+
+            if (!placeDetailsJSON.isNull("formatted_phone_number"))
+                phoneNumber = placeDetailsJSON.getString("formatted_phone_number");
+
+            if (!placeDetailsJSON.isNull("website"))
+                website = placeDetailsJSON.getString("website");
+
+            if (!placeDetailsJSON.isNull("opening_hours")) {
+                placeDetailsJSON = placeDetailsJSON.getJSONObject("opening_hours");
+
+                if (!placeDetailsJSON.isNull("weekday_text")) {
+                    JSONArray weekdayHoursJsonArray = placeDetailsJSON.getJSONArray("weekday_text");
+                    openingHours = new String[WEEKDAYS_NUMBER];
+                    int hoursArrayLength = weekdayHoursJsonArray.length();
+                    String currParsedString;
+
+                    for (int i = 0; i < hoursArrayLength; i++) {
+                        openingHours[i] = "";
+                        currParsedString = (String)weekdayHoursJsonArray.get(i);
+
+                        if ((currParsedString == null) || (!currParsedString.contains(":")))
+                            continue;
+
+                        openingHours[i] = getOpeningHours(currParsedString, i);
+                    }
+                }
+            }
+
+            placeDetails.put(VeterinaryClinicMoreDetailsActivity.VET_LATITUDE, vetLatitude);
+            placeDetails.put(VeterinaryClinicMoreDetailsActivity.VET_LONGITUDE, vetLongitude);
+            placeDetails.put(VeterinaryClinicMoreDetailsActivity.PHONE_NUMBER, phoneNumber);
+            placeDetails.put(VeterinaryClinicMoreDetailsActivity.WEBSITE, website);
+            placeDetails.put(VeterinaryClinicMoreDetailsActivity.OPENING_HOURS, openingHours);
+
+            // TODO take it off
+            System.out.println("Vet latitude is: " + vetLatitude);
+            System.out.println("Vet longitude is: " + vetLongitude);
+            System.out.println("Phone number is: " + phoneNumber);
+            System.out.println("Website is: " + website);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (NullPointerException ignored) {
+        }
+
+        return placeDetails;
+    }
+
+    private static String getOpeningHours(String currParsedString, int index) {
+        switch (index) {
+            case SUNDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+                currParsedString = currParsedString.substring(11);
+                break;
+            case MONDAY:
+            case SATURDAY:
+                currParsedString = currParsedString.substring(9);
+                break;
+            case FRIDAY:
+                currParsedString = currParsedString.substring(10);
+                break;
+            default:
+                currParsedString = null;
+        }
+
+        boolean keepParsing = (currParsedString.contains(":") && currParsedString.contains("–"));
+
+        if (!keepParsing)
+            return currParsedString;
+
+        // TODO change to String Builder
+        String parsedHours = "";
+        int specialCharIndex;
+
+        while (keepParsing) {
+            specialCharIndex = currParsedString.indexOf("–");
+            parsedHours = (currParsedString.substring(0, specialCharIndex) + parsedHours);
+            parsedHours = ("–" + parsedHours);
+            currParsedString = currParsedString.substring(specialCharIndex + 1);
+
+            if (!currParsedString.contains(",")) {
+                parsedHours = (currParsedString.substring(0) + parsedHours);
+                keepParsing = false;
+            } else {
+                specialCharIndex = currParsedString.indexOf(",");
+                parsedHours = (currParsedString.substring(0, specialCharIndex) + parsedHours);
+                parsedHours = ("  ," + parsedHours);
+                currParsedString = currParsedString.substring(specialCharIndex + 2);
+            }
+        }
+
+        return parsedHours;
+    }
+}
--- app/src/main/java/il/ac/tau/cloudweb17a/hasorkim/VetListActivity.java
+++ app/src/main/java/il/ac/tau/cloudweb17a/hasorkim/VetListActivity.java
@@ -1,94 +1,379 @@
 package il.ac.tau.cloudweb17a.hasorkim;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.LayoutManager;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class VetListActivity extends BaseActivity {
+import java.io.IOException;
+import java.util.List;
 
-    private LayoutInflater layoutInflater;
-    private ViewGroup thisContainer;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import static il.ac.tau.cloudweb17a.hasorkim.VetListActivity.QueryType.DISTANCE_SEARCH_ALL;
+import static il.ac.tau.cloudweb17a.hasorkim.VetListActivity.QueryType.DISTANCE_SEARCH_OPEN;
+import static il.ac.tau.cloudweb17a.hasorkim.VetListActivity.QueryType.NEARBY_SEARCH_ALL;
+import static il.ac.tau.cloudweb17a.hasorkim.VetListActivity.QueryType.NEARBY_SEARCH_OPEN;
+
+public class VetListActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String TAG  = VetListActivity.class.getSimpleName();
+    private static final String DEFAULT_LATITUDE  = "32.0820748";  // TODO handle this
+    private static final String DEFAULT_LONGITUDE = "34.7717487"; // TODO handle this
+
+    public static final String PLACE_ID          = "com.example.hasorkim.place_id";
+    public static final String NAME              = "com.example.hasorkim.name";
+    public static final String ADDRESS           = "com.example.hasorkim.address";
+    public static final String ORIGIN_LATITUDE  = "com.example.hasorkim.origin_latitude";
+    public static final String ORIGIN_LONGITUDE = "com.example.hasorkim.origin_longitude";
+
+    // TODO should I declare them here or inside OcCreate
+    private ProgressBar progressBar;
+    private RadioGroup vetTypeButtons;
+    private RadioButton open_vets_button;
+    private LinearLayout vetListLayout;
+    private RecyclerView vetListRecyclerView;
+    private List<VeterinaryClinic> allVetsList;
+    private List<VeterinaryClinic> openVetsList;
+    private String currLatitude  = DEFAULT_LATITUDE;
+    private String currLongitude = DEFAULT_LONGITUDE;
+
+    /********************************** New variables *****************************************/
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private boolean mLocationPermissionGranted = false;
+    /******************************************************************************************/
+
+    public enum QueryType {
+        NEARBY_SEARCH_ALL, NEARBY_SEARCH_OPEN, DISTANCE_SEARCH_ALL, DISTANCE_SEARCH_OPEN
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_vet_list);
+        setContentView(R.layout.activity_vet_list);
 
-        layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        // Connecting to the XML widgets
+        progressBar = findViewById(R.id.vet_list_progress_bar);
+        vetTypeButtons = findViewById(R.id.vet_type_buttons_group);
+        open_vets_button = findViewById(R.id.open_vets_button);
+        vetListLayout = findViewById(R.id.vet_list_layout);
+        vetListRecyclerView = findViewById(R.id.vet_list_recycler_view);
 
-        thisContainer = (ViewGroup) layoutInflater.inflate(R.layout.activity_vet_list, null);
+        // We use this setting to improve performance because changes
+        // in content do not change the layout size of the RecyclerView
+        vetListRecyclerView.setHasFixedSize(true);
 
-        mDrawer.addView(thisContainer, 0);
+        // use a linear layout manager
+        LayoutManager vetListLayoutManager = new LinearLayoutManager(this);
+        vetListRecyclerView.setLayoutManager(vetListLayoutManager);
 
-        Button openVetMapButton = thisContainer.findViewById(R.id.openVetMapButton);
-        openVetMapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(VetListActivity.this, VetListMapActivity.class);
-                startActivity(intent);
-            }
-        });
+        VetListItemDecoration decoration = new VetListItemDecoration(this, Color.LTGRAY, 1f);
+        vetListRecyclerView.addItemDecoration(decoration);
 
-        Button openReportDetailsButton = thisContainer.findViewById(R.id.openReportDetailsButton);
-        openReportDetailsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(VetListActivity.this, NewEventMoreDetailsRequestActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ArrayList<VeterinaryClinic> vetList = new ArrayList<>();
-
-        vetList.add(new VeterinaryClinic("Name1", "Address1",
-                "OpeningHours1"));
-        vetList.add(new VeterinaryClinic("Name2", "Address2",
-                "OpeningHours2"));
-        vetList.add(new VeterinaryClinic("Name3", "Address3",
-                "OpeningHours3"));
-        vetList.add(new VeterinaryClinic("Name4", "Address4",
-                "OpeningHours4"));
-        vetList.add(new VeterinaryClinic("Name5", "Address5",
-                "OpeningHours5"));
-        vetList.add(new VeterinaryClinic("Name6", "Address6",
-                "OpeningHours6"));
-
-        VeterinaryClinicAdapter adapter = new VeterinaryClinicAdapter(
-                this,
-                R.layout.vet_list_item,
-                vetList
-        );
-        ListView listView = (ListView) findViewById(R.id.list_view_vets);
-        listView.setAdapter(adapter);
-
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocationPermission();
     }
 
+    /**
+     * The listener for the "Back to report" button
+     */
+    public void OnButtonClick(View v) {
+        Toast.makeText(getApplicationContext(), "Will return to report", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        Context context = this.getApplicationContext();
+
+        if ((ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION)   == PackageManager.PERMISSION_GRANTED) &&
+            (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)    ) {
+                mLocationPermissionGranted = true;
+                System.out.println("There is permission");
+                getDeviceLocation(); // TODO check is should stay here
+        }
+        else {
+            System.out.println("There is NOT permission");
+            ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
     @Override
-    public void onBackPressed()
-    {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+
+        //mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+
+        getDeviceLocation(); // TODO check is should stay here
+    }
+
+    /**
+     * Get the best and most recent location of the device, which may be null in rare
+     * cases when a location is not available.
+     */
+    private void getDeviceLocation() {
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && (task.getResult() != null)) {
+                            // Set the map's camera position to the current location of the device.
+                            Location mLastKnownLocation = task.getResult();
+                            currLatitude = Double.toString(mLastKnownLocation.getLatitude());
+                            currLongitude = Double.toString(mLastKnownLocation.getLongitude());
+                        }
+                        else {
+                            System.out.println("Current location is null. Using defaults.");
+                            System.out.println("Exception: " + task.getException());
+                        }
+
+                        /*
+                        getAllNearbyVets();  // Getting closest vets
+                        getOpenNearbyVets(); // getting the closest open vets
+                        */
+                        getNearbyVets(false);
+                        getNearbyVets(true);
+                    }
+                });
+            }
+            else
+                // until getting permission
+                getLocationPermission(); // TODO check if should stay at the end
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    /**
+     *
+     */
+    private void getNearbyVets(boolean onlyOpen) {
+        Uri.Builder url = new Uri.Builder()
+                .scheme("https")
+                .authority("maps.googleapis.com")
+                .appendPath("maps")
+                .appendPath("api")
+                .appendPath("place")
+                .appendPath("nearbysearch")
+                .appendPath("json")
+                .appendQueryParameter("location", (currLatitude + "," + currLongitude))
+                .appendQueryParameter("type", "veterinary_care")
+                .appendQueryParameter("rankby", "distance")
+                .appendQueryParameter("language", "iw")
+                .appendQueryParameter("key", getString(R.string.google_places_key));
+
+
+        if (onlyOpen)
+            url.appendQueryParameter("opennow", "true");
+
+        String currentUrl = url.build().toString();
+        Log.d(TAG, currentUrl);
+
+        try {
+            if (onlyOpen)
+                run(currentUrl, NEARBY_SEARCH_OPEN);
+            else
+                run(currentUrl, NEARBY_SEARCH_ALL);
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    /**
+     * @param vetList
+     */
+    private void distancesApiCall(List<VeterinaryClinic> vetList, QueryType distanceSearchType) {
+        int listSize = vetList.size();
+        Log.d(TAG, "Vet list size is: " + listSize);
+
+        StringBuilder nearVetsID = new StringBuilder();
+        for (int i = 0; i < listSize; i++) {
+            nearVetsID.append("place_id:" + vetList.get(i).getPlaceId());
+
+            if (i < (listSize - 1))
+                nearVetsID.append("|");
+        }
+
+        Uri.Builder urlMaps = new Uri.Builder()
+                .scheme("https")
+                .authority("maps.googleapis.com")
+                .appendPath("maps")
+                .appendPath("api")
+                .appendPath("distancematrix")
+                .appendPath("json")
+                .appendQueryParameter("origins", (currLatitude + "," + currLongitude))
+                .appendQueryParameter("destinations", nearVetsID.toString())
+                .appendQueryParameter("mode","driving")
+                .appendQueryParameter("language", "iw")
+                .appendQueryParameter("key", getString(R.string.google_places_key));
+
+        String currentUrlDistances = urlMaps.build().toString();
+        Log.d(TAG, currentUrlDistances);
+
+        try {
+            run(currentUrlDistances, distanceSearchType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param url
+     * @param parseType
+     * @throws IOException
+     */
+    private void run(String url, final QueryType parseType) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    String returnedString = responseBody.string();
+
+                    // delete this 2 lines
+                    System.out.println("RETURNED STRING IS:");
+                    System.out.println(returnedString);
+
+                    switch (parseType) {
+                        case NEARBY_SEARCH_ALL:
+                            allVetsList = DataParser.parsePlaces(returnedString);
+                            distancesApiCall(allVetsList, DISTANCE_SEARCH_ALL);
+                            break;
+
+                        case NEARBY_SEARCH_OPEN:
+                            openVetsList = DataParser.parsePlaces(returnedString);
+                            distancesApiCall(openVetsList, DISTANCE_SEARCH_OPEN);
+                            break;
+
+                        case DISTANCE_SEARCH_ALL:
+                            DataParser.getDistances(returnedString, allVetsList);
+                            break;
+
+                        case DISTANCE_SEARCH_OPEN:
+                            DataParser.getDistances(returnedString, openVetsList);
+                            updateUI(); // calling to update the screen
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    private void updateUI() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final VeterinaryClinicAdapter.OnItemClickListener buttonsListener = new VeterinaryClinicAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(VeterinaryClinic vetClinic) {
+                        Intent intent = new Intent(VetListActivity.this, VeterinaryClinicMoreDetailsActivity.class);
+                        intent.putExtra(PLACE_ID, vetClinic.getPlaceId());
+                        intent.putExtra(NAME, vetClinic.getName());
+                        intent.putExtra(ADDRESS, vetClinic.getAddress());
+                        intent.putExtra(ORIGIN_LATITUDE, currLatitude);
+                        intent.putExtra(ORIGIN_LONGITUDE, currLongitude);
+                        startActivity(intent);
+                    }
+                };
+
+                vetListRecyclerView.setAdapter(new VeterinaryClinicAdapter(allVetsList, buttonsListener));
+
+                vetTypeButtons.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        System.out.println("Got inside cause radio button changed");
+
+                        switch (checkedId) {
+                            case R.id.all_vets_button:
+                                vetListRecyclerView.setAdapter(new VeterinaryClinicAdapter(allVetsList, buttonsListener));
+                                break;
+
+                            case R.id.open_vets_button:
+                                vetListRecyclerView.setAdapter(new VeterinaryClinicAdapter(openVetsList, buttonsListener));
+                        }
+                    }
+                });
+
+                // updating the screen with the open clinics showed
+                vetTypeButtons.clearCheck();
+                open_vets_button.setChecked(true);
+                progressBar.setVisibility(View.GONE);
+                vetListLayout.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    /****************************************************************************************************/
+    @Override
+    public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(VetListActivity.this, ReportEventActivity.class));
         finish();
-
-    }
-
-    public void openReportDetails(View view)
-    {
-        Intent intent = new Intent(VetListActivity.this, NewEventMoreDetailsRequestActivity.class);
-        startActivity(intent);
-    }
-
-    public void openVetMap(View view)
-    {
-        Intent intent = new Intent(VetListActivity.this, VetListMapActivity.class);
-        startActivity(intent);
     }
 }
