261454205191ee1ad201ac7791ca2bcd875492d7
boris <scipiobb2@gmail.com>
2018-01-05 21:04:27 +0200

    phone number validate

--- app/src/main/java/il/ac/tau/cloudweb17a/hasorkim/ActiveReportActivity.java
+++ app/src/main/java/il/ac/tau/cloudweb17a/hasorkim/ActiveReportActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
