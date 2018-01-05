a837ffc1d183c1b50dd757b990dd1682d750d2f6
Bar Schwartz <sch.bar@gmail.com>
2018-01-05 18:01:21 +0200

    Add new report cloud function

--- app/src/cloudFunction/functions/index.js
+++ app/src/cloudFunction/functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/reports/{report}').onUpdate(event => {
+exports.notifyReporterStatusChanged = functions.database.ref('/reports/{report}').onUpdate(event => {
     const report = event.params.report;
 
     const oldStatus = event.data.previous.val().status;
@@ -33,5 +33,48 @@ exports.sendNotification = functions.database.ref('/reports/{report}').onUpdate(
 
     // Send notifications to user.
     return admin.messaging().sendToDevice(userId, payload, options);
+
+});
+
+exports.notifyManagersAndScannersNewReport = functions.database.ref('/reports/{report}').onCreate(event => {
+    const topic = "new_report"
+
+    const report = event.params.report;
+
+    const data_lat = event.data.val().lat;
+    const data_long = event.data.val().long;
+    const data_address = event.data.val().address;
+
+
+    if (!data_lat || !data_long || !data_address) {
+        return;
+    }
+
+    console.log('Report ', report, 'has been created');
+    console.log('lat: ', data_lat, ', long: ', data_long, ', address: ', data_address);
+
+    // Notification details.
+    const payload = {
+        data: {
+            lat: `${data_lat}`,
+            long: `${data_long}`,
+            address: `${data_address}`,
+        }
+    };
+
+
+    var options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+    // Send notifications to user.
+    admin.messaging().sendToTopic(topic, payload, options)
+      .then(function(response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function(error) {
+        console.log("Error sending message:", error);
+      });
 
 });
